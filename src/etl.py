6d0cc858ec3e02ed51264d624735128d7577fe02
Luis Diaz <43221010+luismdiaz01@users.noreply.github.com>
2020-04-12 20:38:19 -0700

    Add files via upload

--- src/etl.py
+++ src/etl.py
@@ -1,10 +1,27 @@
 import pandas as pd
 import os
 
-def get_data(url, outpath = 'raw', title = 'stops'):
+def limit_cols(df, cols):
+    return df[cols]
+
+def get_data(urls, outpath = 'data/raw', title = ['stops', 'arrests', 'crime']):
     if not os.path.exists(outpath):
         os.mkdir(outpath)
-    df = pd.read_csv(url)    
-    name = './' + outpath + '/'+title + '.csv'
-    df.to_csv(name)
-    return 'Files saved in ' + outpath  
+    for j, i in enumerate(urls):    
+        df = pd.read_csv(i)    
+        name = './' + outpath + '/'+title[j] + '.csv'
+        df.to_csv(name, index = False)
+        print("Downloaded: ",name)
+    return 'Files saved in ' + outpath     
+    
+def process(paths, outpath = 'data/cleaned' ,title = ['stops', 'arrests', 'crime'], **kwargs):
+    if not os.path.exists(outpath):
+        os.mkdir(outpath)
+    for j,i in enumerate(paths):
+        df = pd.read_csv(i)
+        if 'cols' in kwargs:
+            df = limit_cols(df, cols[j])
+        name = './' + outpath + '/'+title[j] + '-processed.csv'
+        df.to_csv(name, index = False)
+        print("Downloaded: ",name)        
+    return 'Files saved in ' + outpath   
