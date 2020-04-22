88f7139baf0f8501f87f33800b2ed33f4dc10989
Luis Diaz <43221010+luismdiaz01@users.noreply.github.com>
2020-04-21 17:59:44 -0700

    Add files via upload

--- src/etl.py
+++ src/etl.py
@@ -4,6 +4,24 @@ import os
 def limit_cols(df, cols):
     return df[cols]
 
+def clean_divisions(df, make_dict = True, divs = ['Officer 1 Division Number', 'Officer 2 Division Number'], desc = ['Division Description 1', 'Division Description 2'], name = 'data/cleaned/div.txt'):
+    for j in desc:    
+        if j == 'Division Description 1':
+            df = df.dropna(subset = [j])
+        df = df[~(df[j] =='**UNUSED PIU CODE**')]
+    for i in divs:
+        ser = df[i]
+        ser = ser.astype(str)
+        ser = ser.replace('0','00')
+        ser = ser.str.replace('.0','', regex = False)
+        ser = ser.str.lstrip('0')
+        ser = ser.replace('', '0')
+        ser = pd.to_numeric(df[i], errors = 'ignore')
+        df[i] = ser
+    if make_dict:
+        df[['Officer 1 Division Number','Division Description 1']].drop_duplicates().sort_values('Officer 1 Division Number').to_csv(name, header=None, index=None, sep=' ', mode='a')
+    return df
+
 def get_data(urls, outpath = 'data/raw', title = ['stops', 'arrests', 'crime']):
     if not os.path.exists(outpath):
         os.mkdir(outpath)
@@ -19,9 +37,11 @@ def process(paths, outpath = 'data/cleaned' ,title = ['stops', 'arrests', 'crime
         os.mkdir(outpath)
     for j,i in enumerate(paths):
         df = pd.read_csv(i)
+        if 'stop' in i:
+            df = clean_divisions(df)
         if 'cols' in kwargs:
             df = limit_cols(df, cols[j])
         name = './' + outpath + '/'+title[j] + '-processed.csv'
         df.to_csv(name, index = False)
         print("Downloaded: ",name)        
-    return 'Files saved in ' + outpath   
+    return 'Files saved in ' + outpath     
